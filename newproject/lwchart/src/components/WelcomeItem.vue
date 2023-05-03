314fd292e5a5a6de56b14664d1eee547287d50e3
KimForest91 <forest_91@naver.com>
2023-05-03 13:20:11 +0900

    lightWeight Chart

---
+++ newproject/lwchart/src/components/WelcomeItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import { createChart } from 'lightweight-charts';
+
+    let chart;
+    const chartContainer = ref();
+
+    onMounted(() => {
+        // Create the Lightweight Charts Instance using the container ref.
+        chart = createChart(chartContainer.value);
+    });
+
+    onUnmounted(() => {
+        if (chart) {
+            chart.remove();
+            chart = null;
+        }
+    });
+</script>
+<template>
+    <div class="lw-chart" ref="chartContainer"></div>
+</template>
+<style scoped>
+    .lw-chart {
+        height: 100%;
+    }
+</style>
